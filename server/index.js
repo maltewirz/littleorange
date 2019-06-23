c8eb0235de95dfc0c3bb9212fe8568b2aa46a20f
Malte Wirz <mail@maltewirz.com>
2019-06-23 22:40:46 +0200

    axios not yet installed

--- server/index.js
+++ server/index.js
@@ -1,51 +1,23 @@
 const express = require("express");
+const app = express();
 const path = require("path");
-const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
+const server = require('http').Server(app);
 
-const isDev = process.env.NODE_ENV !== "production";
-const PORT = process.env.PORT || 5000;
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-    console.error(`Node cluster master ${process.pid} is running`);
+// Answer API requests.
+app.get("/api/test", function(req, res) {
+    res.json("successs!");
+});
 
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function(request, response) {
+    response.sendFile(
+        path.resolve(__dirname, "../react-ui/build", "index.html")
+    );
+});
 
-    cluster.on("exit", (worker, code, signal) => {
-        console.error(
-            `Node cluster worker ${
-                worker.process.pid
-            } exited: code ${code}, signal ${signal}`
-        );
-    });
-} else {
-    const app = express();
-
-    // Priority serve any static files.
-    app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
-
-    // Answer API requests.
-    app.get("/api", function(req, res) {
-        res.set("Content-Type", "application/json");
-        res.send('{"message":"Hello from the custom server!"}');
-    });
-
-    // All remaining requests return the React app, so it can handle routing.
-    app.get("*", function(request, response) {
-        response.sendFile(
-            path.resolve(__dirname, "../react-ui/build", "index.html")
-        );
-    });
-
-    app.listen(PORT, function() {
-        console.error(
-            `Node ${
-                isDev ? "dev server" : "cluster worker " + process.pid
-            }: listening on port ${PORT}`
-        );
-    });
-}
+server.listen(process.env.PORT || 5000, () =>
+    console.log("I'm listening on 5000.")
+);
